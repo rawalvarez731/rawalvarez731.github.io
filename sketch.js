7a954a6536822370affe65ea841b0f315a9f07df
Richard Alvarez <richard@thestage>
2017-07-19 19:13:28 -0500

    no collision gang

--- sketch.js
+++ sketch.js
@@ -1,79 +1,119 @@
 'use strict';
 
-let capture;
-let coreyFont;
+const SQUIGLY_RADIUS = 20;
+const SQUIGLY_SPEED = 1;
+var tick;
 
-function randomString(length, chars) {
-    var mask = '';
-    if (chars.indexOf('a') > -1) mask += 'abcdefghijklmnopqrstuvw';
-    if (chars.indexOf('A') > -1) mask += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    if (chars.indexOf('#') > -1) mask += '0123456789';
-    if (chars.indexOf('!') > -1) mask += '~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\';
-    var result = '';
-    for (var i = length; i > 0; --i) result += mask[Math.floor(Math.random() * mask.length)];
-    return result;
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
 }
 
-
-function preload()
+function distance(x1, y1, x2, y2)
 {
-	coreyFont = loadFont("https://rawalvarez731.github.io/WELOC___.TTF");
+	return Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
 }
 
-function squigly(x, y, angle1, angle2)
+class Squigly
 {
-	smooth();
-	noFill();
-	strokeWeight(3.0);
-	stroke("#EDDF46");
-	strokeJoin(MITER);
-	beginShape();
-	vertex(x + Math.cos(angle1)*20, y + Math.sin(angle1)*20);
-	vertex(x, y);
-	vertex(x + Math.cos(angle2)*20, y + Math.sin(angle2)*20);
-	endShape();
-}
+	constructor(x, y, type)
+	{
 
-function circle(x, y)
-{
-	smooth();
-	noFill();
-	strokeWeight(3.0);
-	stroke("#EDDF46");
-	ellipse(x, y, 20);	
-}
+		this.x = x;
+		this.y = y;
 
-let squiglys = [];
-let circles  = [];
-let str      = randomString(5, "a");
-function setup()
-{
-	createCanvas(windowWidth, windowHeight);	
-	for(let i = 0; i < 125; i++){
-		squiglys.push({
-			x: Math.floor(Math.random()*windowWidth - 50) + 50,
-			y: Math.floor(Math.random()*windowHeight - 50) + 50,
-			angle1: Math.random()*Math.PI*2,
-			angle2: Math.random()*Math.PI*2
-		});
-		circles.push({
-			x: Math.floor(Math.random()*windowWidth - 50) + 50,
-			y: Math.floor(Math.random()*windowHeight - 50) + 50
-		});
+		// Render strats
+		let circleSquigly = function()
+		{
+			ellipse(this.x, this.y, SQUIGLY_RADIUS);
+		};
+
+		let lineSquigly = function()
+		{
+			beginShape();
+			vertex(this.x + Math.cos(this.angleOne)*SQUIGLY_RADIUS, this.y + Math.sin(this.angleOne)*SQUIGLY_RADIUS);
+			vertex(this.x, this.y);
+			vertex(this.x + Math.cos(this.angleTwo)*SQUIGLY_RADIUS, this.y + Math.sin(this.angleTwo)*SQUIGLY_RADIUS);
+			endShape();
+		};
+
+
+		if(type === "circle")
+		{
+			this.render = circleSquigly;
+		}
+
+		if(type === "line")
+		{
+			this.render = lineSquigly;
+			this.angleOne = Math.random()*Math.PI*2;
+			this.angleTwo = Math.random()*Math.PI*2;
+			let d = distance(x + Math.cos(this.angleOne)*SQUIGLY_RADIUS, y + Math.sin(this.angleOne)*SQUIGLY_RADIUS,
+						     x + Math.cos(this.angleTwo)*SQUIGLY_RADIUS, y + Math.sin(this.angleTwo)*SQUIGLY_RADIUS);
+			while(d < 15)
+			{
+				this.angleOne = Math.random()*Math.PI*2;
+				this.angleTwo = Math.random()*Math.PI*2;
+				d = distance(x + Math.cos(this.angleOne)*SQUIGLY_RADIUS, y + Math.sin(this.angleOne)*SQUIGLY_RADIUS,
+						     x + Math.cos(this.angleTwo)*SQUIGLY_RADIUS, y + Math.sin(this.angleTwo)*SQUIGLY_RADIUS);
+			}
+		}
+
 	}
 }
 
+let nodes = [];
+let squiglys = [];
+function setup()
+{
+	createCanvas(windowWidth, windowHeight);	
+
+	while(nodes.length < 200)
+	{
+		let x = getRandomArbitrary(SQUIGLY_RADIUS*3, windowWidth - SQUIGLY_RADIUS*3);
+		let y = getRandomArbitrary(SQUIGLY_RADIUS*3, windowHeight - SQUIGLY_RADIUS*3);
+		let valid = true;
+		nodes.forEach((n)=>{
+			if(distance(n.x, n.y, x, y) < SQUIGLY_RADIUS*3.5)
+			{
+				valid = false;
+			}
+		});
+		if(valid){
+			nodes.push({
+				x: x,
+				y: y
+			});
+		}else
+		{
+			continue;
+		}
+	}
+
+	nodes.forEach((n)=>{
+		squiglys.push(new Squigly(n.x, n.y, Math.random()<0.87?'line':'circle'));
+	});
+	
+}
 function draw()
 {
+	tick++;
 	background('#040506');
+	smooth();
+	noFill();
+	strokeWeight(2.0);
+	stroke("white");
+	rect(10, 10, windowWidth-20, windowHeight-20);
+	strokeWeight(5.0);
+	stroke("#EDDF46");
 	squiglys.forEach((s)=>{
-		squigly(s.x, s.y, s.angle1, s.angle2);
+		s.render();
 	});
-	circles.forEach((c)=>{
-		circle(c.x, c.y);
-	});
-	fill(255).strokeWeight(0).textSize(150);
-	textFont(coreyFont);
-	textAlign('center');
-	text(str,windowWidth/2, windowHeight/2);
+	/*nodes.forEach((n)=>{
+		smooth();
+		noFill();
+		stroke("red");
+		strokeWeight(1.0);
+		ellipse(n.x, n.y, SQUIGLY_RADIUS*3.5);
+	});*/
 }
+
