cb2ce056f93d9f22dc282e4b3473f314d2cd438a
Richard Alvarez <richard@thestage>
2017-07-22 23:45:29 -0500

    scales

---
+++ whoops/player.js
@@ -0,0 +1,148 @@
+'use strict';
+
+var PLAYER_TILESHEET;
+const PLAYER_IMAGE_WIDTH  = 64;
+const PLAYER_IMAGE_HEIGHT = 64;
+
+function Player()
+{
+
+	this.angle = 0;
+	this.angleOff = 0; 
+	this.currentFrame = 0;
+	
+	this.SCALE_WIDTH = width*0.04;
+	this.SCALE_HEIGHT = height*0.07;
+
+	this.mass = 1; 
+	this.pos = createVector(width/2, height/2);
+	this.vel = createVector(0);
+	this.acc = createVector(0);
+
+	this.history = [];
+
+	this.animate = false;
+
+	this.applyforce = function(force)
+	{
+		let f = p5.Vector.div(force, this.mass);
+		this.acc.add(f);
+	}
+
+
+	this.update = function()
+	{
+
+		if(player.pos.x < 0)
+		{
+			player.pos.x = width;
+		}else if(player.pos.x > width)
+		{
+			player.pos.x = 0;
+		}
+
+		// Appply forces to position etc...
+
+		let friction = this.vel.copy();
+		friction.mult(-1);
+		friction.normalize();
+		friction.mult(0.25);
+
+		this.applyforce(friction);
+		this.vel.add(this.acc);
+		this.pos.add(this.vel);
+
+		this.acc.mult(0);
+
+		// Calculate angle
+		this.angle += this.angleOff;
+
+		// Animate
+		if(this.animate)
+		{
+			if(!(frameCount%3)){
+				this.history.push({p: createVector(this.pos.x, this.pos.y), tick: 15});
+			}
+
+			// For every 6 frames animate
+			if(!(frameCount%6))
+			{
+				this.currentFrame++;
+			}
+			if(this.currentFrame==4)
+			{
+				this.animate=false;
+				this.currentFrame=0;
+			} 
+		}
+	}
+
+	this.draw = function()
+	{
+
+		push();
+		translate(this.pos.x, this.pos.y);
+		rotate(radians(this.angle));
+		tint([190, 222, 44]);
+		image(PLAYER_TILESHEET, 0, 0,
+					    this.SCALE_WIDTH,
+					    this.SCALE_HEIGHT,  
+					    PLAYER_IMAGE_WIDTH*this.currentFrame, 0, 
+					    PLAYER_IMAGE_WIDTH-2,PLAYER_IMAGE_HEIGHT-5);
+		pop();
+
+		let i = 0;
+		let playercolors = [[190, 222, 44],
+					 	    [49, 162, 238], 
+					 	    [184, 37, 53], 
+					 	    [173, 81, 185]];
+		this.history.forEach((p)=>{
+			//min+=255/this.history.length;
+			p.tick--;
+			if(p.tick == 0)
+			{
+				this.history.splice(this.history.indexOf(p), 1);
+			}
+			push();
+			let c = playercolors[i%4];
+			c.push(map(p.tick, 0, 15, 0, 255))
+			tint(c);
+			translate(p.p.x, p.p.y);
+			rotate(radians(this.angle));
+			image(PLAYER_TILESHEET, 0, 0,
+						    this.SCALE_WIDTH,
+						    this.SCALE_HEIGHT, 
+						    PLAYER_IMAGE_WIDTH*this.currentFrame, 0, 
+						    PLAYER_IMAGE_WIDTH-2,PLAYER_IMAGE_HEIGHT-5);
+			pop();
+			i++;
+		});
+	}
+
+	this.keyPressed = function()
+	{
+		if(keyCode == 32){
+			this.animate = true;
+			this.applyforce(createVector(Math.cos(radians(this.angle+270))*12, Math.sin(radians(this.angle+270))*12));
+		}
+	}
+
+	this.handlePressedKeyboard = function()
+	{
+		if(keyIsDown(RIGHT_ARROW) || keyIsDown(68))
+		{
+			this.angleOff = 4;
+		}
+		if(keyIsDown(LEFT_ARROW) || keyIsDown(65))
+		{
+			this.angleOff = -4;
+		}
+	}
+
+	this.handleReleasedKeyboard = function()
+	{
+		this.angleOff = 0;
+	}
+
+
+}
---
+++ whoops/sketch.js
@@ -0,0 +1,187 @@
+/*
+ * Richard Alvarez
+ * Quick and dirty-ish
+ */
+
+/* TODO
+	- Particles
+	- Bullets
+	- Asteroids? Just general baddies.
+
+*/
+
+'use strict';
+
+var GRAVITY;
+
+function Firestone()
+{	
+	this.scaleSmall = height*0.01;
+	this.scaleBig = height*0.07;
+
+	this.angle = 0;
+
+	this.position = createVector(random(0, width),random(0, 0));
+	this.velocity  = createVector(0);
+	this.acceleration = createVector(0);
+
+	this.size = random(this.scaleSmall, this.scaleBig);
+	this.points = floor(random(5, 15));
+	this.v = [];
+
+	this.mass = map(this.size, this.scaleSmall, this.scaleBig, 1, 10);
+
+	for(let i = 0; i < this.points; i++)
+	{
+		let angle = map(i, 0, this.points, 0, TWO_PI);
+		var x = (random(-this.scaleSmall, this.scaleBig) + this.size) * cos(angle);
+		var y = (random(-this.scaleSmall, this.scaleBig) + this.size) * sin(angle);
+		this.v.push(createVector(x, y));
+	}
+	
+	this.hits = function(firestone)
+	{
+		let d = dist(this.position.x, this.position.y, firestone.position.x, firestone.position.y);
+		if(d < firestone.size + this.size)
+		{
+			return true;
+		}else
+		{
+			return false;
+		}
+	}
+
+	this.hitPlayer = function(player)
+	{
+		// explosion.
+		let d = dist(this.position.x, this.position.y, player.pos.x, player.pos.y);
+		if(d < sqrt((player.SCALE_WIDTH**2)+(player.SCALE_HEIGHT**2))/2 + this.size)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+
+	}
+
+	this.applyforce = function(force)
+	{
+		let f = p5.Vector.div(force, this.mass);
+		this.acceleration.add(f);
+	}
+
+	this.update = function()
+	{
+		let friction = this.velocity.copy();
+		friction.mult(-1);
+		friction.normalize();
+		friction.mult(0.01);
+
+		this.applyforce(friction);
+		this.applyforce(GRAVITY);
+		this.velocity.add(this.acceleration);
+		this.position.add(this.velocity);
+
+		this.acceleration.mult(0);
+
+		this.angle+=map(this.mass, 1, 10, 3, 0.1);
+	}
+
+	this.draw = function()
+	{
+		push();
+		stroke([242, 66, 19]);
+		strokeWeight(6.0);
+		noFill();
+		translate(this.position.x, this.position.y);
+		rotate(radians(this.angle));
+		beginShape();
+		this.v.forEach((vv)=>vertex(vv.x, vv.y));
+		endShape(CLOSE);
+		pop();
+/*		push();
+		translate(this.position.x, this.position.y);
+		stroke(255);
+		fill(255);
+		strokeWeight(1);
+		text(this.mass, 0, 0);
+		text(this.velocity.y, 0, 10);
+		pop();*/
+	}
+
+}
+
+function preload()
+{
+	PLAYER_TILESHEET = loadImage("assets/player.png");
+}
+
+var firestones = [];
+var player;
+function setup()
+{
+
+	GRAVITY = createVector(0,0.05);
+
+	createCanvas(windowWidth, windowHeight);
+	frameRate(60);
+	imageMode(CENTER);
+	for(let i = 0; i < 10; i++)
+	{
+		firestones.push(new Firestone());
+	}
+	player = new Player();
+}
+
+function draw()
+{
+	background(7);
+
+	firestones.forEach((f)=>{
+		if(f.position.y-f.size>height)
+		{
+			firestones.push(new Firestone());	
+			firestones.push(new Firestone());
+			firestones.splice(firestones.indexOf(f), 1);
+			return;
+		}
+		if(f.hitPlayer(player))
+		{
+			firestones.splice(firestones.indexOf(f), 1);
+		};
+		firestones.forEach((fp)=>{
+			if(f.hits(fp) && f != fp)
+			{
+				console.log('hit');
+				let xO;
+				if(f.position.x < fp.position.x){
+					xO = -1;
+				}else
+				{
+					xO = 1;
+				}
+
+				f.applyforce(createVector(xO, 0));
+				fp.applyforce(createVector(-xO, 0));
+			}
+		});
+		f.update();
+		f.draw();
+	});
+
+	player.update();
+	player.draw();
+	player.handlePressedKeyboard();
+}
+
+function keyReleased()
+{
+	player.handleReleasedKeyboard();
+}
+
+function keyPressed()
+{
+	player.keyPressed();
+}
